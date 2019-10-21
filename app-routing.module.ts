68c122f97a8c46fe87b8de70322f5e5f53f2dbe5
karthickd1212 <56832687+karthickd1212@users.noreply.github.com>
2019-10-21 19:35:20 +0530

    Add files via upload

---
+++ app-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  { path: 'dashboard', loadChildren: './dashboard/dashboard.module#DashboardPageModule' },
+  { path: 'application', loadChildren: './application/application.module#ApplicationPageModule' },
+  { path: 'applicationdetail', loadChildren: './applicationdetail/applicationdetail.module#ApplicationdetailPageModule' },
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
